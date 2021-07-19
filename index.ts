d987324a3c92f34ec8e0536e0e052a99bd465caa
A-J Roos <asjas@hey.com>
2021-07-19 20:28:33 +0200

    add a bit of console logging

--- index.ts
+++ index.ts
@@ -54,18 +54,22 @@ export function createPrismaRedisCache({ model, cacheTime }, opts) {
       // We need to create a cache that contains enough information to cache the data correctly
       // The cache key looks like this: User_findUnique_{"where":{"email":"alice@prisma.io"}}
       const cacheKey = `${params.model}_${params.action}_${args}`;
+      console.log(cacheKey);
 
       // Try to retrieve the data from the cache first
       result = JSON.parse(await redis.get(cacheKey));
+      console.log("result found in cache", result);
 
       if (result === null) {
         result = await next(params);
 
         // Set the cache with our query
         await redis.setex(cacheKey, cacheTime, JSON.stringify(result));
+        console.log("manually return result and update cache", result);
       }
     } else {
       // Any Prisma action not defined will fall through to here
+      console.log("getting into the else handler", result);
       result = await next(params);
     }
 
