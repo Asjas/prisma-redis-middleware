e4130a1082541ea67a047f96488678d4db4f4f0a
A-J Roos <asjas@hey.com>
2022-02-09 22:23:54 +0200

    add excludeCacheMethods array option to skip caching for certain methods

--- index.ts
+++ index.ts
@@ -2,23 +2,23 @@ import Redis from "ioredis";
 import debug from "debug";
 
 export type PrismaAction = "findUnique" | "findFirst" | "findMany" | "queryRaw" | "aggregate" | "count" | "groupBy";
+export type RedisOptions = Redis.Redis;
 
 /**
  * These options are being passed in to the middleware as "params"
  * https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#params
  */
-// type ModelName = "";
 
-// export type MiddlewareParams = {
-//   model?: ModelName;
-//   action: PrismaAction;
-//   args: any;
-//   dataPath: string[];
-//   runInTransaction: boolean;
-// };
+export type MiddlewareParams<ModelType> = {
+  model?: ModelType;
+  action: PrismaAction;
+  args: any;
+  dataPath: string[];
+  runInTransaction: boolean;
+};
 
 // https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#use
-// export type Middleware<T = any> = (params: any, next: (params: any) => Promise<T>) => Promise<T>;
+export type Middleware<T = any> = (params: any, next: (params: any) => Promise<T>) => Promise<T>;
 
 debug("prisma-redis-middleware");
 
@@ -26,12 +26,37 @@ function log(message: string) {
   debug.log(`[prisma:redis:middleware][DEBUG] ${message}`);
 }
 
-export function createPrismaRedisCache({ model, cacheTime }, opts) {
-  return async function prismaCacheMiddleware(params: any, next: (params: any) => Promise<any>) {
-    const redis = new Redis({ host: opts.REDIS_HOST, port: opts.REDIS_PORT, password: opts.REDIS_AUTH });
+export function createPrismaRedisCache({
+  model,
+  cacheTime,
+  redis,
+  excludeCacheMethods = [],
+}: {
+  model: any;
+  cacheTime: number;
+  redis: Redis.Redis;
+  excludeCacheMethods?: PrismaAction[];
+}) {
+  return async function prismaCacheMiddleware(params: MiddlewareParams<any>, next: (params: any) => Promise<any>) {
     let result;
+    const defaultCacheMethods: PrismaAction[] = [
+      "findUnique",
+      "findFirst",
+      "findMany",
+      "queryRaw",
+      "aggregate",
+      "count",
+      "groupBy",
+    ];
+    const excludedCacheMethods = defaultCacheMethods.filter(
+      (cacheMethod: PrismaAction) => !excludeCacheMethods.includes(cacheMethod),
+    );
 
-    if (params.model === model) {
+    console.log(params);
+    console.log("excludedCacheMethods", excludedCacheMethods);
+    console.log("!excludedCacheMethods.includes(params.action)", !excludedCacheMethods.includes(params.action));
+
+    if (params.model === model && !excludedCacheMethods.includes(params.action)) {
       const args = JSON.stringify(params.args);
 
       // We need to create a cache that contains enough information to cache the data correctly
