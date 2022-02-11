5ff8b4433a1810d632716af95528d496f64c8550
A-J Roos <asjas@hey.com>
2022-02-11 14:03:14 +0200

    Add test to confirm data is being cached

---
+++ test/index.test.ts
@@ -0,0 +1,39 @@
+import ioredismock from "ioredis-mock";
+import tap from "tap";
+import { createPrismaRedisCache } from "../";
+
+// Create the mock Redis instance we need
+const redis = new ioredismock();
+
+tap.test("should get and set items in Redis cache", async ({ equal }) => {
+  // Do some setup stuff
+  const dbValue = "result";
+  const model = "User";
+  const action = "findUnique";
+  const args = { where: { foo: "bar" } };
+  const cacheTime = 100; // 100ms
+  const cacheKey = `${model}:${action}:${JSON.stringify(args)}`;
+  const next = () => Promise.resolve(dbValue);
+
+  const middleware = createPrismaRedisCache({
+    models: [model],
+    redis,
+    cacheTime,
+  });
+
+  // Run a "fake" Prisma database call
+  await middleware(
+    {
+      args,
+      action,
+      model,
+      dataPath: [],
+      runInTransaction: false,
+    },
+    next,
+  );
+
+  // Test if the data exists in the cache
+  equal(JSON.parse(await redis.get(cacheKey)), dbValue);
+});
+
