ce12403d0c0a98875b20d227c40e5b7dd6e29ea4
A-J Roos <asjas@hey.com>
2022-02-11 14:03:58 +0200

    Add test to confirm action is ignored when specified

--- test/index.test.ts
+++ test/index.test.ts
@@ -37,3 +37,35 @@ tap.test("should get and set items in Redis cache", async ({ equal }) => {
   equal(JSON.parse(await redis.get(cacheKey)), dbValue);
 });
 
+tap.test("should exclude Prisma Action from being cached in Redis cache", async ({ equal }) => {
+  // Do some setup stuff
+  const dbValue = "result";
+  const model = "User";
+  const action = "findFirst";
+  const args = { where: { foo: "bar" } };
+  const cacheTime = 100; // 100ms
+  const cacheKey = `${model}:${action}:${JSON.stringify(args)}`;
+  const next = () => Promise.resolve(dbValue);
+
+  const middleware = createPrismaRedisCache({
+    models: [model],
+    redis,
+    cacheTime,
+    excludeCacheMethods: [action],
+  });
+
+  // Run a "fake" Prisma database call
+  await middleware(
+    {
+      args,
+      action,
+      model,
+      dataPath: [],
+      runInTransaction: false,
+    },
+    next,
+  );
+
+  // Test if the data does not exist in the cache
+  equal(JSON.parse(await redis.get(cacheKey)), null);
+});
