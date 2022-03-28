8d8500c09d8dc60245144d6dec8bcebfa0b52b49
A-J Roos <asjas@hey.com>
2022-03-28 18:10:55 +0200

    Write test to confirm that multiple models are cached

--- test/index.test.ts
+++ test/index.test.ts
@@ -44,3 +44,52 @@ test("should cache a single Prisma model", async () => {
   expect(JSON.parse((await redis.get(cacheKey)) as string)).toMatchObject(dbValue);
 });
 
+test("should get and set multiple Prisma models in Redis cache", async () => {
+  // Do some setup stuff
+  const dbValue = { key: "test result" };
+  const model1 = "User";
+  const model2 = "Post";
+  const action = "findUnique";
+  const args = { where: { foo: "bar" } };
+  const cacheTime = 2000; // 2 seconds
+  const cacheKey1 = `User~{"params":{"action":"findUnique","args":{"where":{"foo":"bar"}},"dataPath":[],"model":"User","runInTransaction":false}}`;
+  const cacheKey2 = `Post~{"params":{"action":"findUnique","args":{"where":{"foo":"bar"}},"dataPath":[],"model":"Post","runInTransaction":false}}`;
+  const next = () => Promise.resolve(dbValue);
+
+  const middleware = createPrismaRedisCache({
+    models: [
+      { model: model1, cacheTime },
+      { model: model2, cacheTime },
+    ],
+    storage: { type: "redis", options: { client: redis } },
+  });
+
+  // Run a "fake" User Prisma query
+  await middleware(
+    {
+      args,
+      action,
+      model: model1,
+      dataPath: [],
+      runInTransaction: false,
+    },
+    next,
+  );
+
+  // Run a "fake" Post Prisma query
+  await middleware(
+    {
+      args,
+      action,
+      model: model2,
+      dataPath: [],
+      runInTransaction: false,
+    },
+    next,
+  );
+
+  // Test if the data exists in the cache
+  expect(JSON.parse((await redis.get(cacheKey1)) as string)).toMatchObject(dbValue);
+  expect(JSON.parse((await redis.get(cacheKey2)) as string)).toMatchObject(dbValue);
+});
+
