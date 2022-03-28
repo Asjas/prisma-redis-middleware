382cfa0424b1b128e65b0cc39c2758ae200d52f9
A-J Roos <asjas@hey.com>
2022-03-28 18:30:23 +0200

    Update invalidation test to confirm only 1 model is removed from cache

--- test/index.test.ts
+++ test/index.test.ts
@@ -226,11 +226,13 @@ test("should exclude Prisma model from being cached with defaultExcludeCacheMode
 test("should invalidate a single Prisma model cache after data mutation", async () => {
   // Do some setup stuff
   const dbValue = { key: "test result" };
-  const model = "User";
-  const action = "findFirst";
+  const model1 = "User";
+  const model2 = "Post";
+  const action = "findUnique";
   const args = { where: { foo: "bar" } };
   const defaultCacheTime = 2000; // 2 seconds
-  const cacheKey = `User~{"params":{"action":"findFirst","args":{"where":{"foo":"bar"}},"dataPath":[],"model":"User","runInTransaction":false}}`;
+  const cacheKey1 = `User~{"params":{"action":"findUnique","args":{"where":{"foo":"bar"}},"dataPath":[],"model":"User","runInTransaction":false}}`;
+  const cacheKey2 = `Post~{"params":{"action":"findUnique","args":{"where":{"foo":"bar"}},"dataPath":[],"model":"Post","runInTransaction":false}}`;
   const next = () => Promise.resolve(dbValue);
 
   const middleware = createPrismaRedisCache({
@@ -243,7 +245,19 @@ test("should invalidate a single Prisma model cache after data mutation", async 
     {
       args,
       action,
-      model,
+      model: model1,
+      dataPath: [],
+      runInTransaction: false,
+    },
+    next,
+  );
+
+  // Run a "fake" Post Prisma query
+  await middleware(
+    {
+      args,
+      action,
+      model: model2,
       dataPath: [],
       runInTransaction: false,
     },
@@ -251,14 +265,15 @@ test("should invalidate a single Prisma model cache after data mutation", async 
   );
 
   // Test if data exists in the Redis cache
-  expect(JSON.parse((await redis.get(cacheKey)) as string)).toMatchObject(dbValue);
+  expect(JSON.parse((await redis.get(cacheKey1)) as string)).toMatchObject(dbValue);
+  expect(JSON.parse((await redis.get(cacheKey2)) as string)).toMatchObject(dbValue);
 
   // Run a "fake" User Prisma mutation
   await middleware(
     {
       args,
       action: "create",
-      model,
+      model: model1,
       dataPath: [],
       runInTransaction: false,
     },
@@ -266,5 +281,6 @@ test("should invalidate a single Prisma model cache after data mutation", async 
   );
 
   // Test if the cache was invalidated and cleared properly
-  assert.equal(JSON.parse((await redis.get(cacheKey)) as string), null);
+  assert.equal(JSON.parse((await redis.get(cacheKey1)) as string), null);
+  expect(JSON.parse((await redis.get(cacheKey2)) as string)).toMatchObject(dbValue);
 });
