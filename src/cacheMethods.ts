b3b9e8f552b94c4acca69b629e489ab2fd0c26a2
Filipe Macedo <devmacedo@gmail.com>
2022-08-17 11:46:19 -0300

    refactor: remove duplicate value

--- src/cacheMethods.ts
+++ src/cacheMethods.ts
@@ -13,7 +13,6 @@ export const defaultCacheMethods: PrismaQueryAction[] = [
 
 export const defaultMutationMethods: PrismaMutationAction[] = [
   "create",
-  "create",
   "createMany",
   "update",
   "updateMany",
